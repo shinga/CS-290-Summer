d351591a2a047cd32aa350be61ff33a3e4ef07e3
Arthur Shing <arthur.shing@gmail.com>
2016-08-06 17:14:25 -0700

    removing node_modules

--- FINAL PROJECT/final_project.js
+++ FINAL PROJECT/final_project.js
@@ -1,17 +1,11 @@
 var express = require('express');
+var mysql = require('./sqlstuff.js');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 var bodyParser = require('body-parser');
 
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'student',
-  password: 'default',
-  database: 'exercises'
-});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,11 +14,48 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-// app.set('port', 302);
+app.set('port', 3000);
 
-app.get('/',function(req,res){
-    res.render('home.handlebars') //We can omit the .handlebars extension as we do below
+app.get('/',function(req,res,next){
+    var context = {};
+    mysql.pool.query('SELECT * FROM exercises', function(err, rows, fields){
+        if(err){
+            return next(err);
+        }
+        context.results = JSON.stringify(rows);
+        res.render('home.handlebars', context);
+    });
 });
+
+app.get('/reset',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS exercises", function(err){
+    var createString = "CREATE TABLE exercises("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  mysql.pool.query("INSERT INTO exercises (`name`) VALUES (?)", [req.query.c], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.render('home',context);
+  });
+});
+
 
 function genContext(){
     var stuffToDisplay = {};
